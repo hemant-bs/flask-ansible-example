7e2eb6ad9e29e375f9aec91c85c0666326eeb3cd
Hemant-bs <hemant.y@browserstack.com>
2022-07-13 19:56:59 +0530

    updated app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello():
 
 @app.route("/health")
 def health():
-    return jsonify({'status': 'ok'})
+    return jsonify({'status': 'ok','name':'hemant'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
